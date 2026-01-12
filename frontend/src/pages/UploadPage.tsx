5b2d49bb98397ef65ea0003b775f8c6b6d4dc6e1
Daffa F <daffafirdaus@saptasarana.com>
2026-01-12 14:52:23 +0700

    removed useless applied role 2

--- frontend/src/pages/UploadPage.tsx
+++ frontend/src/pages/UploadPage.tsx
@@ -185,7 +185,6 @@ const UploadPage = () => {
 
       // Reset form
       setCandidateName('');
-      setAppliedRole('');
       setInterviewDate(undefined);
       setFinalStatus(undefined);
       setSelectedClient('');
